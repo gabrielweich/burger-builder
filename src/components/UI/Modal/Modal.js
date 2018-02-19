4cbbfbecc4e9f19f361a87e15a36afef15fdc789
Gabriel Weich <gabrielweich@hotmail.com>
2018-02-19 16:08:13 -0300

    added price in order summary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,15 +6,15 @@ import Auxiliary from '../../../hoc/Auxiliary';
 
 const modal = (props) => (
   <Auxiliary>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-  <div
-  className={classes.Modal}
-  style={{
-    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-    opacity : props.show ? '1' : '0',
-  }}>
-    {props.children}
-  </div>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0',
+      }}>
+      {props.children}
+    </div>
   </Auxiliary>
 );
 
