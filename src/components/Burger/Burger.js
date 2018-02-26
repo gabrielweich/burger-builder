ea4098305197a75a8977599593c5f4d2ef905bb1
Gabriel Weich <gabrielweich@hotmail.com>
2018-02-26 10:44:43 -0300

    Order history

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,21 +6,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
   let transformedIngredients = [];
 
-  Object.keys(props.ingredients).map(igKey => {
-    for(let i =0; i< parseInt(props.ingredients[igKey], 10); i++){
-      transformedIngredients.push(<BurgerIngredient key = {igKey + i} type = {igKey}/>)
+  for (let igKey in props.ingredients) {
+    for (let i = 0; i < parseInt(props.ingredients[igKey], 10); i++) {
+      transformedIngredients.push(<BurgerIngredient key={igKey + i} type={igKey} />)
     }
-  })
+  }
 
-  if(transformedIngredients.length === 0){
+  if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>
   }
 
-  return(
+  return (
     <div className={classes.Burger}>
-      <BurgerIngredient type="bread-top"/>
+      <BurgerIngredient type="bread-top" />
       {transformedIngredients}
-      <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 }
