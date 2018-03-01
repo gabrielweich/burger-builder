a9fb93ebbf61b6e1d904476b27bf25edc6541df0
Gabriel Weich <gabrielweich@hotmail.com>
2018-03-01 19:55:49 -0300

    redux integration in checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    price: 0,
-  }
-
-  componentWillMount() {
-    const query = new URLSearchParams(this.props.location.search);
-    const ingredients = {};
-    let totalPrice = 0;
-    for (let param of query.entries()) {
-      if (param[0] === 'price') {
-        totalPrice = param[1];
-      } else {
-        ingredients[param[0]] = param[1];
-      }
-    }
-
-    this.setState({ ingredients, totalPrice });
-  }
-
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   }
@@ -40,13 +21,19 @@ class Checkout extends Component {
         <CheckoutSummary
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
-          ingredients={this.state.ingredients} />
+          ingredients={this.props.ingredients} />
         <Route
           path={this.props.match.url + '/contact-data'}
-          render={(props) => <ContactData {...props} ingredients={this.state.ingredients} price={this.state.totalPrice} />} />
+          component={ContactData} />
       </div>
     )
   }
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+  }
+}
+
+export default connect(mapStateToProps)(Checkout);
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
@@ -104,7 +105,7 @@ class ContactData extends Component {
     
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price,
+      price: this.props.totalPrice,
       orderData: formData,
     }
 
@@ -195,4 +196,10 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice,
+  }
+}
+export default connect(mapStateToProps)(ContactData);
